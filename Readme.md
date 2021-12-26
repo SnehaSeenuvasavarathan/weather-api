4b75cd485d46a16a1290578f3d0f0e4f6a5bca8f
Sneha Seenuvasavarathan <snehaseenuvasavarathan@gmail.com>
2021-12-25 19:38:41 -0600

    first commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+## Weather API
+
+In this project I learnt how to use APIs and fetch data. The site takes the city as input and displays the weather information
---
+++ app.js
@@ -0,0 +1,32 @@
+const express=require("express")
+const app=express()
+const https=require("https")
+const bodyparser=require("body-parser")
+app.use(bodyparser.urlencoded({extended:true}))
+app.get("/", function (req,res) {
+  res.sendFile(__dirname+"/index.html")
+
+})
+app.post("/", function(req,res){
+  city=req.body.city;
+  url="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=c550ad18ebe744a4669026ac832ea7e0&units=metric"
+  console.log(city)
+  https.get(url, function (response) {
+  console.log(response);
+  response.on("data", function(data){
+        weather=JSON.parse(data);
+        console.log(weather);
+        console.log(weather.main.temp);
+        const icon=weather.weather[0].icon;
+        const source = "http://openweathermap.org/img/wn/"+icon+"@2x.png"
+        res.write('<head><meta charset="utf-8"></head>');
+        res.write("<img src="+source+ ">")
+        res.write("<h1>The temperature at "+city+" is "+weather.main.temp+" degrees<\h1>")
+        res.send();
+    })
+
+  })
+})
+app.listen(3000, function () {
+  console.log("listening on port 3000");
+})
